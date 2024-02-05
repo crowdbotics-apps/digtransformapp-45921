bee322cb418ad7a28f6bebee28993df1353905bd
Crowdbotics <team@crowdbotics.com>
2024-02-05 16:42:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "digtransformapp-45921",
+  url: "https://digtransformapp-45921.botics.co",
+  api: "https://digtransformapp-45921.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
